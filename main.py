4e099906140815ffa5fbaec8c39309168571947c
jkahudvc <jkahulugan772@insite.4cd.edu>
2024-07-08 17:38:49 -0700

    a

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 
 def getInput():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    nums = list(input("Please enter as many integers as you can: ").split())
+    for i in range(len(nums)):
+        nums[i] = int(nums[i])
+    return nums
 
 
 def makeReverse(numbers):
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    l = len(numbers)
+    for i in range(0,int(l/2)):
+        numbers[i],numbers[l-1-i] = numbers[l-1-i], numbers[i]
+    return numbers
+        
 
 
 def main():
